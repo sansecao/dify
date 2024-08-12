22426c9440e23562207a4c602d309d0da53e007f
houxueping001 <houxueping001@ke.com>
2024-08-12 15:34:33 +0800

    修改代码结构

--- api/core/workflow/nodes/start/entities.py
+++
@@ -1,9 +0,0 @@
-from core.app.app_config.entities import VariableEntity
-from core.workflow.entities.base_node_data_entities import BaseNodeData
-
-
-class StartNodeData(BaseNodeData):
-    """
-    Start Node Data
-    """
-    variables: list[VariableEntity] = []
--- api/services/enterprise/enterprise_service.py
+++
@@ -1,8 +0,0 @@
-from services.enterprise.base import EnterpriseRequest
-
-
-class EnterpriseService:
-
-    @classmethod
-    def get_info(cls):
-        return EnterpriseRequest.send_request('GET', '/info')
